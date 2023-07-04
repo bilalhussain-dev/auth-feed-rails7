a5a39ec4ccbcbc978793b19d1517ff0adf961794
Bilal Hussain <bilalhussian785@gmail.com>
2023-07-04 12:33:57 +0500

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+        redirect_to @post, notice: 'Comment was successfully created.'
+    else
+      redirect_to @post, alert: 'Error creating comment.'
+    end
+  end
+
+
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
